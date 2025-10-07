0e7dc8fb1dafbde2daf18928e128e9d48282e2cb
HuangFei <trongphi.hoang2511@gmail.com>
2025-10-07 22:15:02 +0700

    update link

--- src/main.js
+++ src/main.js
@@ -239,7 +239,7 @@ document.querySelectorAll("#ui-overlay .menu-item").forEach(item => {
         } else if (link === "#contact") {
             targetUrl = "https://www.linkedin.com/in/hoangtrongphi2511/";
         } else if (link === "#about") {
-            targetUrl = "https://drive.google.com/drive/u/0/folders/1TRbTRjFWWdXCvMOJLnZYTyfH07sgigSo";
+            targetUrl = "https://drive.google.com/file/d/1Hc0S3Zq1E6DpHCv6L61jFMq-QXU-Rtvp/view?usp=drive_link";
         }
 
         gsap.to(item, {
